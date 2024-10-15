7346d3b864f9cd1a5bb9c835a9539979cab1f544
yodoj <nadiraalia4@gmail.com>
2024-10-15 17:32:37 +0700

    Menambahkan CSV pada database dan membuat fitur explore

--- explore/filters.py
+++ explore/filters.py
@@ -5,3 +5,4 @@ class MenuFilter(django_filters.FilterSet):
     class Meta:
         model = Menu
         fields = ['menu', 'category', 'price', 'restaurant_name', 'city', 'price', 'rating', 'specialized']
+        
