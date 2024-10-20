a7933e2ffb0ea238f9c34c1b4379f0b2cb7728ef
halizaarfa <halizansa@gmail.com>
2024-10-20 10:04:45 +0700

    First push of spin the wheel: implemented up to wheel and options

--- spinthewheel/models.py
+++ spinthewheel/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from explore.models import Menu
 
+# Option to be put on the wheel
 class Option(models.Model):
     menu = models.OneToOneField(Menu, on_delete=models.CASCADE)
     added = models.BooleanField(default=False)
 
+# Save history after spinning
 class SpinHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     winner = models.TextField()
--- spinthewheel/views.py
+++ spinthewheel/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from explore.models import Menu
 from .models import Option, SpinHistory
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
-import json
 
 @login_required(login_url='/login')
 def menu_view(request):
